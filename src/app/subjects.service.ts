9bd5a4adc2e401cdcdb3a9d837b640e1eca2df6a
Luca-Clemente <lukpinkman@hotmail.com>
2022-04-01 22:25:25 -0300

    1 file modified

--- src/app/subjects.service.ts
+++ src/app/subjects.service.ts
@@ -1,17 +1,6 @@
 import { Injectable } from '@angular/core';
-interface Subject {
-  title: string;
-}
 
 @Injectable()
 export class SubjectsService {
-  list: Array<Subject> = [];
-
-
-  constructor() { }
-
- 
-  }
-
-
+  constructor() {}
 }
